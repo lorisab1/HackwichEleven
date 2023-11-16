709ecbae37fcea05a488b9788ec60cf7c9369333
Lorisa Bulosan <lorisab@hawaii.edu>
2023-11-16 13:34:50 -1000

    Completed HW11 Part 1

--- HackwichEleven/ViewController.swift
+++ HackwichEleven/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider!
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func myGuessButtonPressed(_ sender: Any)
+    {
+        let message = "The value is: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderHasMoved(_ sender: Any)
+    {
+        print("The value of the slider is:\(slider.value)")
+        currentValue = Int((slider.value))
+    }
+    
+    
 }
 
